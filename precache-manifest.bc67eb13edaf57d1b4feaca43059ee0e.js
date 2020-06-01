39debea777d152bcf8388d04ba75ca85331849bf
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-06-01 10:39:32 +0200

    Updates

---
+++ precache-manifest.bc67eb13edaf57d1b4feaca43059ee0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f98a50a15e43e9a844af2cf3e90775",
+    "url": "/Picture-React-App/index.html"
+  },
+  {
+    "revision": "bebe31a7958b4aaab1c1",
+    "url": "/Picture-React-App/static/css/main.b6636a97.chunk.css"
+  },
+  {
+    "revision": "075450067036af49ea93",
+    "url": "/Picture-React-App/static/js/2.daf1dd78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Picture-React-App/static/js/2.daf1dd78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bebe31a7958b4aaab1c1",
+    "url": "/Picture-React-App/static/js/main.31cdcf66.chunk.js"
+  },
+  {
+    "revision": "d9d13aeed8f2a6a99cf0",
+    "url": "/Picture-React-App/static/js/runtime-main.b0702ddf.js"
+  }
+]);
